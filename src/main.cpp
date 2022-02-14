35065e4346bf5554320ee561751f9682bca56b9d
GarrenSouza <contato.garren@gmail.com>
2022-02-14 12:32:05 -0300

    wip: CLI application is under testing

--- src/main.cpp
+++ src/main.cpp
@@ -10,20 +10,14 @@ using namespace cv;
 using namespace Local;
 
 int main(int argc, char **argv) {
-    QApplication app(argc, argv);
-    QWidget window;
-    window.resize(320, 240);
-    window.show();
-    window.setWindowTitle(QApplication::translate("toplevel", "Main Window"));
-    return app.exec();
     // cv::Mat base_image;
 
-    // Local::UserInput userInput;
+    Local::UserInput userInput;
 
-    // if (argc < 2) {
-    //     printf("Usage: app.out <Image_Path>\n");
-    //     return -1;
-    // }
+    if (argc < 2) {
+        printf("Usage: app.out <Image_Path>\n");
+        return -1;
+    }
 
     // base_image = cv::imread(argv[1], 1);
     // if (!base_image.data) {
@@ -31,16 +25,64 @@ int main(int argc, char **argv) {
     //     return -1;
     // }
 
-    // cv::namedWindow("Base Image", WINDOW_AUTOSIZE);
-    // cv::namedWindow("Transformed Image", WINDOW_AUTOSIZE);
+    Local::Image base_image = Image(std::string(argv[1]));
+    Local::Image processing_image = Image(base_image);
+    std::cout << processing_image.info() << std::endl;
 
-    // Local::Image processing_image = Image(std::string(argv[1]));
-    // std::cout << processing_image.info() << std::endl;
-    // processing_image.toGrayscale();
-    // cv::imshow("Base Image", base_image);
-    // cv::imshow("Transformed Image", processing_image.underlyingContainer());
+    bool exit = false;
+    char option = '0';
+    while (!exit) {
+        std::cout << "|----[ Garren's Image Manipulation Tool ]----|" << std::endl;
+        std::cout << "| Enter the operation:" << std::endl;
+        std::cout << "| g. convert to grayscale" << std::endl;
+        std::cout << "| v. flip verticaly" << std::endl;
+        std::cout << "| h. flip horizontally" << std::endl;
+        std::cout << "| q. quantize" << std::endl;
+        std::cout << "| r. reset to base image" << std::endl;
+        std::cout << "| s. see result" << std::endl;
+        std::cout << "| j. save to JPEG" << std::endl;
+        std::cout << "| e. exit" << std::endl;
+        option = userInput.getUserInput("Option: ", std::cin)[0];
+        switch (option) {
+        case 'e':
+            exit = true;
+            break;
 
-    // waitKey(0);
-    // // processing_image.saveToDisk(std::string(argv[3]));
-    // return 0;
+        case 'v': // flips vertically
+            processing_image.mirrorV();
+            break;
+
+        case 'h': // flips horizontally
+            processing_image.mirrorH();
+            break;
+
+        case 'g': // converts to grayscale
+            processing_image.toGrayscale();
+            break;
+
+        case 'q': // quantize
+            processing_image.toGrayscale().quantize(std::stoi(userInput.getUserInput("| Tones in target set: ", std::cin)));
+            break;
+
+        case 'j': // save to JPEG file
+            processing_image.saveToDisk(userInput.getUserInput("| Enter the file path: ", std::cin));
+            break;
+
+        case 'r': // resets to base image
+            processing_image = base_image;
+            break;
+
+        case 's': // see result
+            cv::namedWindow("Base Image", WINDOW_AUTOSIZE);
+            cv::namedWindow("Transformed Image", WINDOW_AUTOSIZE);
+            cv::imshow("Base Image", base_image.underlyingContainer());
+            cv::imshow("Transformed Image", processing_image.underlyingContainer());
+            waitKey(0);
+            break;
+        default:
+            std::cout << ">Invalid option!" << std::endl;
+            break;
+        }
+    }
+    return 0;
 }
