aec5de1ff66bbe96030a007866da79069d1dc729
GarrenSouza <contato.garren@gmail.com>
2022-02-14 09:24:33 -0300

    feat: successfully added Qt5 dependencies + demo code

--- src/main.cpp
+++ src/main.cpp
@@ -4,36 +4,43 @@
 
 #include "Image.hpp"
 #include "Input.hpp"
+#include <QtWidgets>
 
 using namespace cv;
 using namespace Local;
 
 int main(int argc, char **argv) {
-    cv::Mat base_image;
+    QApplication app(argc, argv);
+    QWidget window;
+    window.resize(320, 240);
+    window.show();
+    window.setWindowTitle(QApplication::translate("toplevel", "Main Window"));
+    return app.exec();
+    // cv::Mat base_image;
 
-    Local::UserInput userInput;
+    // Local::UserInput userInput;
 
-    if (argc < 2) {
-        printf("Usage: app.out <Image_Path>\n");
-        return -1;
-    }
+    // if (argc < 2) {
+    //     printf("Usage: app.out <Image_Path>\n");
+    //     return -1;
+    // }
 
-    base_image = cv::imread(argv[1], 1);
-    if (!base_image.data) {
-        printf("No image data \n");
-        return -1;
-    }
+    // base_image = cv::imread(argv[1], 1);
+    // if (!base_image.data) {
+    //     printf("No image data \n");
+    //     return -1;
+    // }
 
-    cv::namedWindow("Base Image", WINDOW_AUTOSIZE);
-    cv::namedWindow("Transformed Image", WINDOW_AUTOSIZE);
+    // cv::namedWindow("Base Image", WINDOW_AUTOSIZE);
+    // cv::namedWindow("Transformed Image", WINDOW_AUTOSIZE);
 
-    Local::Image processing_image = Image(std::string(argv[1]));
-    std::cout << processing_image.info() << std::endl;
-    processing_image.toGrayscale();
-    cv::imshow("Base Image", base_image);
-    cv::imshow("Transformed Image", processing_image.underlyingContainer());
+    // Local::Image processing_image = Image(std::string(argv[1]));
+    // std::cout << processing_image.info() << std::endl;
+    // processing_image.toGrayscale();
+    // cv::imshow("Base Image", base_image);
+    // cv::imshow("Transformed Image", processing_image.underlyingContainer());
 
-    waitKey(0);
-    processing_image.saveToDisk(std::string(argv[3]));
-    return 0;
+    // waitKey(0);
+    // // processing_image.saveToDisk(std::string(argv[3]));
+    // return 0;
 }
